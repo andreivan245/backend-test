ba7661c0171c2ff1828fc41cca2cd736aae0e34e
andreivan245 <andreflus887@gmail.com>
2024-09-23 11:06:42 -0300

    feat: added createInvestment in service

--- backend-test/src/main/java/com/andre/backend_test/service/InvestmentService.java
+++ backend-test/src/main/java/com/andre/backend_test/service/InvestmentService.java
@@ -15,6 +15,10 @@ public class InvestmentService {
    private InvestmentRepository investmentRepository;
 
     public Investment createInvestment(InvestmentDTO investmentDTO){
+        if (!newInvestmentIsValid(investmentDTO.getCreationDate(), investmentDTO.getValue())){
+            throw new RuntimeException("Invalid investment");
+        }
+
         Investment entityInvestment = new Investment(investmentDTO);
         investmentRepository.save(entityInvestment);
 
@@ -31,5 +35,13 @@ public class InvestmentService {
 
     public void getListInvestments(){
 
+    }
+
+    public boolean newInvestmentIsValid(Date date, Double value){
+        Date today = new Date();
+
+        if(today.before(date) || value < 0) return false;
+
+        return true;
     }
 }
